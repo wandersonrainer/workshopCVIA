305e990b10764d07e53ab8fff796e0958df8a7b5
wandersonrainer <wandersonrainer@icloud.com>
2025-06-30 20:02:37 -0300

    Update prog00_webcams.py

--- prog00_webcams.py
+++ prog00_webcams.py
@@ -1,1 +1,9 @@
+import cv2
 
+for i in range(6):  #testa as cameras de 0 a 5
+    cap = cv2.VideoCapture(i)
+    if cap.isOpened():
+        print(f"Webcam encontrada no indice: {i}")
+        cap.release()
+    else:
+        print(f"Nenhuma webcam no indice: {i}")
